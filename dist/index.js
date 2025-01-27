83e5252f741bc2cd2ace6a768f3e8d764281bc83
Guilherme Messias <messiasguilherme700@gmail.com>
2025-01-27 04:40:44 -0300

    Adiciona lógica de manipulação de formulário para seleção de empresa e exibição de mensagens correspondentes

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,34 @@
-const companyForm = document.querySelector('#company-form');
-companyForm.addEventListener('submit', (event) => {
+const companyForm = document.querySelector("#company-form");
+companyForm.addEventListener("submit", (event) => {
     event.preventDefault();
     const formData = new FormData(companyForm);
     const company = Object.fromEntries(formData.entries());
-    console.log(company);
+    switch (company["company"]) {
+        case "company-1":
+            console.log("Empresa 1");
+            break;
+        case "company-2":
+            console.log("Empresa 2");
+            break;
+        case "company-3":
+            console.log("Empresa 3");
+            break;
+        case "company-4":
+            console.log("Empresa 4");
+            break;
+        case "company-5":
+            console.log("Empresa 5");
+            break;
+        case "company-6":
+            console.log("Empresa 6");
+            break;
+        case "company-7":
+            console.log("Empresa 7");
+            break;
+        case "company-8":
+            console.log("Empresa 8");
+            break;
+        default:
+            console.log("Empresa não encontrada");
+    }
 });
