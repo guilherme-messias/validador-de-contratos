4e6fafb60d1e022ff2ef3c307d690d91e6834367
Guilherme Messias <messiasguilherme700@gmail.com>
2025-01-29 05:36:08 -0300

    Adiciona lógica para ocultar botão de nova consulta e captura valores no formulário de detalhes do contrato

--- clients/company-1/index.ts
+++ clients/company-1/index.ts
@@ -1,13 +1,15 @@
-const btnNewQuery = document.getElementById('btn-new-query') as HTMLButtonElement;
+const btnNewQuery = document.getElementById("btn-new-query") as HTMLButtonElement;
+btnNewQuery.classList.add("hidden");
 
-btnNewQuery.classList.add('hidden');
-
-const contractType = (document.getElementById('contract-type') as HTMLSelectElement).value;
-const workModality = (document.getElementById('work-modality') as HTMLSelectElement).value;
-const careerLevel = (document.getElementById('career-level') as HTMLSelectElement).value;
-const isAdvocate = (document.querySelector('input[name="flag"]:checked') as HTMLSelectElement).value;
-const position = (document.getElementById('position') as HTMLSelectElement).value;
+let result = "";
 
 document.getElementById("contract-details-form").addEventListener("submit", (event) => {
   event.preventDefault();
+
+  const contractType = (document.getElementById("contract-type") as HTMLSelectElement).value;
+  const workModality = (document.getElementById("work-modality") as HTMLSelectElement).value;
+  const careerLevel = (document.getElementById("career-level") as HTMLSelectElement).value;
+  const isAdvocate = (document.querySelector('input[name="flag"]:checked') as HTMLSelectElement)
+    .value;
+  const position = (document.getElementById("position") as HTMLSelectElement).value;
 });
