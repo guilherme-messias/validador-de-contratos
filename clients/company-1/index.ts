ced9cc7b492903f4cd6eb8667db5c10c71e8f625
Guilherme Messias <messiasguilherme700@gmail.com>
2025-01-29 05:12:05 -0300

    Adiciona lógica para ocultar botão de nova consulta e captura valores dos campos de seleção no formulário da empresa 1

---
+++ clients/company-1/index.ts
@@ -0,0 +1,9 @@
+const btnNewQuery = document.getElementById('btn-new-query') as HTMLButtonElement;
+
+btnNewQuery.classList.add('hidden');
+
+const contractType = (document.getElementById('contract-type') as HTMLSelectElement).value;
+const workModality = (document.getElementById('work-modality') as HTMLSelectElement).value;
+const careerLevel = (document.getElementById('career-level') as HTMLSelectElement).value;
+const isAdvocate = (document.querySelector('input[name="flag"]:checked') as HTMLSelectElement).value;
+const position = (document.getElementById('position') as HTMLSelectElement).value;
