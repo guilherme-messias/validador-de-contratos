de3289a13f49b7de4ee62a5fe29a455916a57654
Guilherme Messias <messiasguilherme700@gmail.com>
2025-02-01 13:53:35 -0300

    Adiciona função contractHomeOffice para gerar contratos de trabalho para teletrabalho com base no nível de carreira e tipo de contrato

---
+++ clients/company-1/filters/contractHomeOffice.ts
@@ -0,0 +1,24 @@
+export default function contractHomeOffice(careerLevel: string, contractType: string): string {
+  let result = "";
+
+  if (
+    careerLevel !== "career-level-specialist-II" &&
+    careerLevel !== "career-level-coordinator" &&
+    contractType !== "contract-temporary"
+  ) {
+    result += `emp - **CONTRATO DE TRABALHO - TELETRABALHO(30514)
+
+    `;
+  }
+
+  if (
+    (careerLevel === "career-level-specialist-II" || careerLevel === "career-level-coordinator") &&
+    contractType !== "contract-temporary"
+  ) {
+    result += `emp - **CONTRATO DE TRABALHO - TELETRABALHO CARGO CONFIANÇA 60/40 (30515)
+      
+      `;
+  }
+
+  return result;
+}
