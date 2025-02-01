47e53886ee6634d5ba6183053f614b2998f684a8
Guilherme Messias <messiasguilherme700@gmail.com>
2025-02-01 13:59:36 -0300

    Adiciona função contractModalityOffice para gerar contratos de trabalho com base no nível de carreira e tipo de contrato

---
+++ dist/company-1/filters/contractModalityOffice.js
@@ -0,0 +1,30 @@
+export default function contractModalityOffice(careerLevel, contractType) {
+    let result = "";
+    if (careerLevel !== "career-level-specialist-II" &&
+        careerLevel !== "career-level-coordinator" &&
+        contractType !== "contract-temporary") {
+        result += `emp - **CONTRATO DE TRABALHO - INDETERMINADO (30511)
+    
+    `;
+    }
+    if ((careerLevel === "career-level-specialist-II" || careerLevel === "career-level-coordinator") &&
+        contractType !== "contract-temporary") {
+        result += `emp - CONTRATO DE TRABALHO - INDETERMINADO CARGO CONFIANÇA 60/40 (30513)
+      
+      `;
+    }
+    if (careerLevel !== "career-level-specialist-II" &&
+        careerLevel !== "career-level-coordinator" &&
+        contractType === "contract-temporary") {
+        result += `emp - **CONTRATO DE TRABALHO - DETERMINADO (30508)
+      
+      `;
+    }
+    if ((careerLevel === "career-level-specialist-II" || careerLevel === "career-level-coordinator") &&
+        contractType === "contract-temporary") {
+        result += `emp - **CONTRATO DE TRABALHO - DETERMINADO CARGO CONFIANÇA 60/40 (30510)
+      
+      `;
+    }
+    return result;
+}
