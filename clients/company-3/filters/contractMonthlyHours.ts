d2016a44d934008b27d7b6c9c20961b553555535
Guilherme Messias <messiasguilherme700@gmail.com>
2025-02-03 05:24:09 -0300

    Atualiza a lógica de filtragem de horas mensais para utilizar identificadores mais descritivos

--- clients/company-3/filters/contractMonthlyHours.ts
+++ clients/company-3/filters/contractMonthlyHours.ts
@@ -1,17 +1,17 @@
 export default function contractMonthlyHours(monthlyHours: string): string {
   let result = "";
 
-  if (monthlyHours === "200") {
+  if (monthlyHours === "monthly-hours-200") {
     result += `emp - CONTRATO 45/45 JORNADA 40 HORAS/200 MENSAIS - MELHOR ENVIO (36718)
     `;
   }
 
-  if (monthlyHours === "180") {
+  if (monthlyHours === "monthly-hours-180") {
     result += `emp - CONTRATO 45/45 JORNADA 36 HORAS/180 MENSAIS - MELHOR ENVIO (36719)
     `;
   }
 
-  if (monthlyHours === "150") {
+  if (monthlyHours === "monthly-hours-150") {
     result += `emp - CONTRATO 45/45 JORNADA 30 HORAS / 150 MENSAIS - MELHOR ENVIO (36717)
     `;
   }
