2c806d416312bc321db0ed43f31c08bc2fe1741a
Guilherme Messias <messiasguilherme700@gmail.com>
2025-02-09 14:44:54 -0300

    Atualiza as funções de modalidade de contrato para retornar um array com códigos de contrato e kit de admissão, melhorando a estrutura de retorno.

--- dist/company-5/filters/contractModalityHomeOffice.js
+++ dist/company-5/filters/contractModalityHomeOffice.js
@@ -1,15 +1,16 @@
 export default function contractModalityHomeOffice(careerLevel, contractType) {
     let contractCodes = "";
+    let admissionKit = "";
     if (careerLevel !== "career-level-specialist-II" &&
         careerLevel !== "career-level-coordinator" &&
         contractType !== "contract-temporary") {
-        contractCodes += `emp - **CONTRATO DE TRABALHO - TELETRABALHO(30514)
-    `;
+        contractCodes += `emp - **CONTRATO DE TRABALHO - TELETRABALHO(30514)\n`;
+        admissionKit += `Titulo: COLABORADOR | HOME-OFFICE\n`;
     }
     if ((careerLevel === "career-level-specialist-II" || careerLevel === "career-level-coordinator") &&
         contractType !== "contract-temporary") {
-        contractCodes += `emp - **CONTRATO DE TRABALHO - TELETRABALHO CARGO CONFIANÇA 60/40 (30515)   
-      `;
+        contractCodes += `emp - **CONTRATO DE TRABALHO - TELETRABALHO CARGO CONFIANÇA 60/40 (30515)\n`;
+        admissionKit += `Titulo: COLABORADOR | HOME-OFFICE | 60/40\n`;
     }
-    return contractCodes;
+    return [contractCodes, admissionKit];
 }
