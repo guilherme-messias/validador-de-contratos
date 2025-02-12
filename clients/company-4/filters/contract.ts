6a08e0842ce8dde88068112809391a670518c57e
Guilherme Messias <messiasguilherme700@gmail.com>
2025-02-12 04:55:32 -0300

    Atualiza a lógica de categorização de contratos para incluir novas condições de modalidade de trabalho e ajusta o HTML removendo a opção 'Presencial'.

--- clients/company-4/filters/contract.ts
+++ clients/company-4/filters/contract.ts
@@ -13,38 +13,68 @@ export default function contract(
     contractType !== "contract-inter"
   ) {
     if (department === "department-other") {
-      if (workModality === "modality-hybrid" || careerLevel === "career-level-others") {
+      if (workModality === "modality-hybrid" && careerLevel === "career-level-others") {
         contractCodes += `emp - CONTRATO HÍBRIDO - OCTADESK (34524)\n`;
         admissionKit += `Titulo: COLABORADOR | HIBRIDO\n`;
       }
 
-      if (workModality === "modality-hybrid" || careerLevel === "career-level-others") {
+      if (workModality === "modality-home-office" && careerLevel === "career-level-others") {
         contractCodes += `emp - CONTRATO TELETRABALHO - OCTADESK (33169)\n`;
         admissionKit += `Titulo: COLABORADOR | HOME-OFFICE\n`;
       }
     }
 
     if (department === "department-i") {
-      if (workModality === "modality-hybrid" || careerLevel === "career-level-others") {
+      if (workModality === "modality-hybrid" && careerLevel === "career-level-others") {
         contractCodes += `emp - CONTRATO HÍBRIDO - OCTADESK (34524)\n`;
         admissionKit += `Titulo: COLABORADOR | HIBRIDO | PRE VENDAS\n`;
       }
 
-      if (workModality === "modality-hybrid" || careerLevel === "career-level-others") {
+      if (workModality === "modality-home-office" && careerLevel === "career-level-others") {
         contractCodes += `emp - CONTRATO TELETRABALHO - OCTADESK (33169)\n`;
         admissionKit += `Titulo: COLABORADOR | HOME-OFFICE | PRE VENDAS\n`;
       }
     }
 
     if (department === "department-ii") {
-      if (workModality === "modality-hybrid" || careerLevel === "career-level-others") {
+      if (workModality === "modality-hybrid" && careerLevel === "career-level-others") {
         contractCodes += `emp - CONTRATO HÍBRIDO - OCTADESK (34524)\n`;
         admissionKit += `Titulo: COLABORADOR | HIBRIDO | VENDAS\n`;
       }
 
-      if (workModality === "modality-hybrid" || careerLevel === "career-level-others") {
+      if (workModality === "modality-home-office" && careerLevel === "career-level-others") {
         contractCodes += `emp - CONTRATO TELETRABALHO - OCTADESK (33169)\n`;
         admissionKit += `Titulo: COLABORADOR | HOME-OFFICE | VENDAS\n`;
+      }
+    }
+  }
+
+  if (
+    careerLevel === "career-level-coordinator" ||
+    careerLevel === "career-level-manager" ||
+    careerLevel === "career-level-tech-leader"
+  ) {
+    if (department === "department-other") {
+      if (workModality === "modality-hybrid") {
+        contractCodes += `emp - CONTRATO HÍBRIDO - OCTADESK (34524)\n`;
+        admissionKit += `Titulo: COLABORADOR | HIBRIDO | 60/40\n`;
+      }
+
+      if (workModality === "modality-home-office") {
+        contractCodes += `emp - CONTRATO TELETRABALHO - OCTADESK (33169)\n`;
+        admissionKit += `Titulo: COLABORADOR | HOME-OFFICE | 60/40\n`;
+      }
+    }
+
+    if (department === "department-ii") {
+      if (workModality === "modality-hybrid" && careerLevel === "career-level-coordinator") {
+        contractCodes += `emp - CONTRATO HÍBRIDO - OCTADESK (34524)\n`;
+        admissionKit += `Titulo: COLABORADOR | HIBRIDO | 60/40\n`;
+      }
+
+      if (workModality === "modality-home-office" && careerLevel === "career-level-coordinator") {
+        contractCodes += `emp - CONTRATO TELETRABALHO - OCTADESK (33169)\n`;
+        admissionKit += `Titulo: COLABORADOR | HOME-OFFICE | 60/40\n`;
       }
     }
   }
