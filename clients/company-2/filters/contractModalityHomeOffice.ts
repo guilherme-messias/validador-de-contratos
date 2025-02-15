5377d49677fe18b1fa471263742ea3669147021d
Guilherme Messias <messiasguilherme700@gmail.com>
2025-02-15 14:48:04 -0300

    Atualiza a lógica de montagem do kit de admissão para incluir informações sobre o estado e o departamento, melhorando a clareza das informações retornadas.

--- clients/company-2/filters/contractModalityHomeOffice.ts
+++ clients/company-2/filters/contractModalityHomeOffice.ts
@@ -13,21 +13,25 @@ export default function contractModalityHomeOffice(
     contractType !== "contract-temporary"
   ) {
     contractCodes += `emp - **CONTRATO DE TRABALHO - TELETRABALHO(30514)\n`;
-    admissionKit +=
-      state === "state-curitiba"
-        ? `Titulo: COLABORADOR | HOME-OFFICE | CURITIBA\n`
-        : `Titulo: COLABORADOR | HOME-OFFICE | OUTROS\n`;
+    admissionKit += `Titulo: COLABORADOR | HOME-OFFICE`;
   }
 
   if (
     (careerLevel === "career-level-specialist-II" || careerLevel === "career-level-coordinator") &&
     contractType !== "contract-temporary"
   ) {
-    contractCodes += `emp - **CONTRATO DE TRABALHO - TELETRABALHO CARGO CONFIANÇA 60/40 (30515)\n`;
-    admissionKit +=
-      state === "state-curitiba"
-        ? `Titulo: 60/40 | HOME-OFFICE | CURITIBA\n`
-        : `Titulo: 60/40 | HOME-OFFICE | OUTROS\n`;
+    contractCodes += `emp - **CONTRATO DE TRABALHO - TELETRABALHO CARGO CONFIANÇA 60/40 (30515)`;
+    admissionKit += `Titulo: 60/40 | HOME-OFFICE`;
+  }
+
+  if (state === "state-curitiba") {
+    admissionKit += ` | CURTIBA`;
+  } else {
+    admissionKit += ` | OUTROS`;
+  }
+
+  if (department !== "department-others") {
+    admissionKit += ` | VENDAS`;
   }
 
   return [contractCodes, admissionKit];
