61c65e87e89060dccb01b57a117f94bb26101426
Guilherme Messias <messiasguilherme700@gmail.com>
2025-02-15 15:05:03 -0300

    Remove quebras de linha desnecessárias nas mensagens de contrato para melhorar a formatação.

--- dist/company-2/filters/termOfWork.js
+++ dist/company-2/filters/termOfWork.js
@@ -3,14 +3,11 @@ export default function termOfWork(contractType, careerLevel, department) {
     if (contractType !== "contract-apprentice" &&
         contractType !== "contract-inter" &&
         department !== "department-others") {
-        contractCodes += `TERMO ADITIVO AO CONTRATO DE TRABALHO - CONNECTPLUG - 36544
-     `;
+        contractCodes += `TERMO ADITIVO AO CONTRATO DE TRABALHO - CONNECTPLUG - 36544\n`;
     }
     if (careerLevel === "career-level-coordinator" || careerLevel === "career-level-manager") {
-        contractCodes += `emp - TERMOS GESTORES - CPLUG (36697) - COORDENACAO E GERENCIA
-    `;
+        contractCodes += `emp - TERMOS GESTORES - CPLUG (36697) - COORDENACAO E GERENCIA\n`;
     }
-    contractCodes += `emp - TERMOS COLABORADORES - CPLUG (36667)
-  `;
+    contractCodes += `emp - TERMOS COLABORADORES - CPLUG (36667)\n`;
     return contractCodes;
 }
