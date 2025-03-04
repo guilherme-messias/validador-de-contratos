f3a4658c1b2f3e17aed7711b586a84a85b1d797e
Guilherme Messias <messiasguilherme700@gmail.com>
2025-03-04 15:00:45 -0300

    corrige caminhos de arquivos no HTML e remove filtros obsoletos de contratos

---
+++ main.js
@@ -0,0 +1,34 @@
+const companyForm = document.querySelector("#company-form");
+companyForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(companyForm);
+  const company = Object.fromEntries(formData.entries());
+  switch (company["company"]) {
+    case "company-1":
+      window.location.href = "./dist/company-1/index.html";
+      break;
+    case "company-2":
+      window.location.href = "./company-2/index.html";
+      break;
+    case "company-3":
+      window.location.href = "./company-3/index.html";
+      break;
+    case "company-4":
+      window.location.href = "./company-4/index.html";
+      break;
+    case "company-5":
+      window.location.href = "./company-5/index.html";
+      break;
+    case "company-6":
+      window.location.href = "./company-6/index.html";
+      break;
+    case "company-7":
+      window.location.href = "./company-7/index.html";
+      break;
+    case "company-8":
+      window.location.href = "./company-8/index.html";
+      break;
+    default:
+      alert("Empresa não encontrada");
+  }
+});
